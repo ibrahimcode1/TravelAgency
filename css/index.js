b775b7806b47875cb4ac60e3f1f0289085b2aeb6
ibrahimcode1 <147648432+ibrahimcode1@users.noreply.github.com>
2023-10-29 23:05:13 +0500

    Add files via upload

---
+++ css/index.js
@@ -0,0 +1,39 @@
+const preloader = document.querySelector('[data-preloader]')
+
+window.addEventListener("load", () =>{
+    preloader.classList.add('remove')
+}) 
+
+
+
+// add event on multiple elements
+
+const addEventOnElements = function (elements, eventTpye, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventTpye, callback);
+        
+    }
+}
+
+// navbar toggler for mobile
+
+const navbar = document.querySelector('[data-navbar]');
+const navTogglers = document.querySelectorAll('[data-nav-toggler]');
+const overlay = document.querySelector('[data-overlay]');
+
+const toggleNav = function () {
+    console.log('toggleNav function is called')
+    navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.body.classList.toggle('nav-active');
+}
+
+addEventOnElements(navTogglers, 'click', toggleNav);
+
+// header
+
+const header = document.querySelector('[data-header]')
+
+window.addEventListener('scroll', function (){
+    header.classList[window.scrollY > 100 ? "add" : "remove"]('active');
+})
